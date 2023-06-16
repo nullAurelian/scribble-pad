a83810bc6afb3fe106eb3146b3b7050dc15fbd94
Eugene Shih <eugene.shih.1@gmail.com>
2023-06-16 10:41:44 -0500

    removed octokit

--- message.js
+++ message.js
@@ -1,15 +1,13 @@
-import { request } from "@octokit/request";
+//import { request } from "@octokit/request";
 
 // const octokit = new request({
 //      auth: `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`,
 //      baseUrl: "https://api.github.com",
 // }); //Expires 2023/06/21
 
-const result = await request(
-    "GET /users/nullAurelian/repos", 
-    {headers: {authorization: "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" }}
-    );
-console.log(result);
+const result = await fetch("https://api.github.com/users/nullAurelian/repos");
+const reposJson = await result.json();
+console.log(reposJson);
 // async function getThings(){
 //     const repos = await octokit.request('GET /user',{})
 //     console.log(repos);
