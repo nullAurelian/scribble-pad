a3002491e3ae5d9098332ad343888625c1948f46
Eugene Shih <eugene.shih.1@gmail.com>
2023-06-16 10:30:36 -0500

    try new request format

--- message.js
+++ message.js
@@ -1,8 +1,18 @@
 import { request } from "@octokit/request";
 
-const octokit = new request({ auth: `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`}) //Expires 2023/06/21
+// const octokit = new request({
+//      auth: `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`,
+//      baseUrl: "https://api.github.com",
+// }); //Expires 2023/06/21
 
-const repos = await octokit.request('GET /users/nullAurelian/repos',{})
+const result = await request(
+    "GET /users/nullAurelian/repos", 
+    {headers: {authorization: "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" }}
+    );
+console.log(result);
+// async function getThings(){
+//     const repos = await octokit.request('GET /user',{})
+//     console.log(repos);
+// }
 
-//alert(repos);
-console.log(repos);
+// getThings();
