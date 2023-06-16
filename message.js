4f84eeab942030c9ff0887d9dd065827188b34df
Eugene Shih <eugene.shih.1@gmail.com>
2023-06-15 20:53:58 -0500

    Try to pull github json for all repos for nullAurelian

--- message.js
+++ message.js
@@ -1,3 +1,8 @@
+import { Octokit } from "https://esm.sh/@octokit/core";
+
+const octokit = new Octokit({})
+
+let repos = await octokit.request('GET /users/nullAurelian/repos',{})
 let testvariable = '';
 
-alert('Hello World');
+alert(repos);
