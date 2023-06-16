3e139815c5322335d19645d90a7bede1f4176297
Eugene Shih <eugene.shih.1@gmail.com>
2023-06-15 21:15:21 -0500

    attempt to resolve octokit request 2

--- message.js
+++ message.js
@@ -1,6 +1,6 @@
-//import { request } from "@octokit/request";
+import { request } from "@octokit/request";
 
-const octokit = new Octokit({ auth: `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`}) //Expires 2023/06/21
+const octokit = new request({ auth: `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`}) //Expires 2023/06/21
 
 const repos = await octokit.request('GET /users/nullAurelian/repos',{})
 
